1ca6fda5d5bd5084f002fb712a445f2318f4ddb7
Raul r <prueba@correo.com>
2026-03-30 00:41:11 -0300

    "Subir paginas de estetica"

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,17 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dentovital.cl',
-  base: '/prueba'
+  base: '/prueba',
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name].[hash][extname]',
+          chunkFileNames: 'chunks/[name].[hash].js',
+          entryFileNames: 'entry/[name].[hash].js'
+        }
+      }
+    }
+  }
 });
